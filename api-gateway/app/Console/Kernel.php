97c357775055fcbd7760978d5f979500e61c82d4
Patrick Falize <patrick@deinternetjongens.nl>
2019-03-28 15:58:32 +0100

    add stress test to api

--- api-gateway/app/Console/Kernel.php
+++ api-gateway/app/Console/Kernel.php
@@ -24,6 +24,21 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->call(function () {
+            $count = 700000;
+
+            $time_start = microtime(true);
+            $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
+            foreach ($stringFunctions as $key => $function) {
+                if (!function_exists($function)) unset($stringFunctions[$key]);
+            }
+            $string = "the quick brown fox jumps over the lazy dog";
+            for ($i=0; $i < $count; $i++) {
+                foreach ($stringFunctions as $function) {
+                    $r = call_user_func_array($function, array($string));
+                }
+            }
+            return number_format(microtime(true) - $time_start, 3);
+        })->everyMinute();
     }
 }
