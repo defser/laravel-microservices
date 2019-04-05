e38f4df9c04d3694ecfac180d159841df376043d
Patrick Falize <patrick@deinternetjongens.nl>
2019-04-06 01:16:36 +0200

    update entrypoint

--- docker/api-gateway/php-apache/entrypoint.sh
+++ docker/api-gateway/php-apache/entrypoint.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
+
 set -e
 
-# Change www-data's uid & guid to be the same as directory in host
-# Fix cache problems
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
+
 usermod -u `stat -c %u /var/www/html` www-data || true
 groupmod -g `stat -c %g /var/www/html` www-data || true
 
 ( cd /var/www/html ; composer install )
 
-if [ "$1" = 'apache2-foreground' ]; then
-    # let's start as root
-    exec "$@"
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+fi
+
+if [ "$role" = "app" ]; then
+
+    exec apache2-foreground
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90
+
+elif [ "$role" = "scheduler" ]; then
+
+    while [ true ]
+    do
+      php /var/www/html/artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
+
 else
-    # change to user www-data
-    su www-data -s /bin/bash -c "$*"
+    echo "Could not match the container role \"$role\""
+    exit 1
 fi
