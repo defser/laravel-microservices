6ffa845218d32fb365ff1e164d2302d5f6fc90e3
Patrick Falize <patrick@deinternetjongens.nl>
2019-04-06 16:36:39 +0200

    fix flooders

--- api-gateway/app/Console/Kernel.php
+++ api-gateway/app/Console/Kernel.php
@@ -58,7 +58,7 @@ class Kernel extends ConsoleKernel
             );
 
             return $number;
-        })->everyFiveMinutes()->name('cpu-flooder-1')->withoutOverlapping();
+        })->everyFiveMinutes()->name('cpu-flooder-first')->withoutOverlapping();
 
         $schedule->call(function () {
             Log::info(
@@ -93,7 +93,7 @@ class Kernel extends ConsoleKernel
             );
 
             return $number;
-        })->everyMinute()->name('cpu-flooder-2')->withoutOverlapping();
+        })->everyMinute()->name('cpu-flooder-second')->withoutOverlapping();
 
         $schedule->call(function () {
             Log::info(
@@ -106,7 +106,7 @@ class Kernel extends ConsoleKernel
 
             $x = str_repeat(
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin turpis ut augue lacinia at ullamcorper dolor condimentum. Nunc elementum suscipit laoreet. Phasellus vel sem justo, a vulputate arcu. Sed rutrum elit nec elit lobortis ultrices. Quisque elit nulla, rutrum et varius sit amet, pulvinar eget purus. Aliquam erat volutpat. Fusce turpis lectus, vestibulum sed ornare sed, facilisis sit amet lacus. Nunc lobortis posuere ultricies. Phasellus aliquet cursus gravida. Curabitur eu erat ac augue rutrum mattis. Suspendisse sit amet urna nec velit commodo feugiat. Maecenas vulputate dictum diam, eu tempor erat volutpat in. Donec id nulla tortor, nec iaculis nibh. Pellentesque scelerisque nisl sit amet ligula dictum commodo. Donec porta mi in lorem porttitor id suscipit lacus auctor.',
-                125000
+                225000
             );
 
             Log::info(
@@ -116,6 +116,6 @@ class Kernel extends ConsoleKernel
                     'MEMORY FLOODER'
                 )
             );
-        })->everyTenMinutes()->name('ram-flooder-1')->withoutOverlapping();
+        })->everyTenMinutes()->name('ram-flooder-first')->withoutOverlapping();
     }
 }
