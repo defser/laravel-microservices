3ac54fd206fb1a4708e0819a56d14c9062a8ddec
Patrick Falize <patrick@deinternetjongens.nl>
2019-04-06 16:46:07 +0200

    fix flooders

--- api-gateway/app/Console/Kernel.php
+++ api-gateway/app/Console/Kernel.php
@@ -94,28 +94,5 @@ class Kernel extends ConsoleKernel
 
             return $number;
         })->everyMinute()->name('cpu-flooder-second')->withoutOverlapping();
-
-        $schedule->call(function () {
-            Log::info(
-                sprintf(
-                    '[%f] Command started: %s',
-                    microtime(true),
-                    'MEMORY FLOODER'
-                )
-            );
-
-            $x = str_repeat(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sollicitudin turpis ut augue lacinia at ullamcorper dolor condimentum. Nunc elementum suscipit laoreet. Phasellus vel sem justo, a vulputate arcu. Sed rutrum elit nec elit lobortis ultrices. Quisque elit nulla, rutrum et varius sit amet, pulvinar eget purus. Aliquam erat volutpat. Fusce turpis lectus, vestibulum sed ornare sed, facilisis sit amet lacus. Nunc lobortis posuere ultricies. Phasellus aliquet cursus gravida. Curabitur eu erat ac augue rutrum mattis. Suspendisse sit amet urna nec velit commodo feugiat. Maecenas vulputate dictum diam, eu tempor erat volutpat in. Donec id nulla tortor, nec iaculis nibh. Pellentesque scelerisque nisl sit amet ligula dictum commodo. Donec porta mi in lorem porttitor id suscipit lacus auctor.',
-                225000
-            );
-
-            Log::info(
-                sprintf(
-                    '[%f] Command ended: %s',
-                    microtime(true),
-                    'MEMORY FLOODER'
-                )
-            );
-        })->everyTenMinutes()->name('ram-flooder-first')->withoutOverlapping();
     }
 }
