e95a6f820032e7c568ea8abef18b4e4564d62a8c
Patrick Falize <patrick@deinternetjongens.nl>
2019-10-27 12:03:35 +0100

    disable example logging and cpu flooders

--- api-gateway/app/Console/Kernel.php
+++ api-gateway/app/Console/Kernel.php
@@ -25,74 +25,74 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            Log::info(
-                sprintf(
-                    '[%f] Command started: %s',
-                    microtime(true),
-                    'CPU FLOODER'
-                )
-            );
-
-            $count = 10000000;
-
-            $time_start = microtime(true);
-            $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
-            foreach ($stringFunctions as $key => $function) {
-                if (!function_exists($function)) unset($stringFunctions[$key]);
-            }
-            $string = "the quick brown fox jumps over the lazy dog";
-            for ($i=0; $i < $count; $i++) {
-                foreach ($stringFunctions as $function) {
-                    $r = call_user_func_array($function, array($string));
-                }
-            }
-            $number = number_format(microtime(true) - $time_start, 3);
-
-            Log::info(
-                sprintf(
-                    '[%f] Command ended: %s',
-                    microtime(true),
-                    'CPU FLOODER'
-                )
-            );
-
-            return $number;
-        })->everyFiveMinutes()->name('cpu-flooder-first')->withoutOverlapping();
-
-        $schedule->call(function () {
-            Log::info(
-                sprintf(
-                    '[%f] Command started: %s',
-                    microtime(true),
-                    'CPU FLOODER 2'
-                )
-            );
-
-            $count = 5000000;
-
-            $time_start = microtime(true);
-            $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
-            foreach ($stringFunctions as $key => $function) {
-                if (!function_exists($function)) unset($stringFunctions[$key]);
-            }
-            $string = "the quick brown fox jumps over the lazy dog";
-            for ($i=0; $i < $count; $i++) {
-                foreach ($stringFunctions as $function) {
-                    $r = call_user_func_array($function, array($string));
-                }
-            }
-            $number = number_format(microtime(true) - $time_start, 3);
-
-            Log::info(
-                sprintf(
-                    '[%f] Command ended: %s',
-                    microtime(true),
-                    'CPU FLOODER 2'
-                )
-            );
-
-            return $number;
-        })->everyMinute()->name('cpu-flooder-second')->withoutOverlapping();
+//        $schedule->call(function () {
+//            Log::info(
+//                sprintf(
+//                    '[%f] Command started: %s',
+//                    microtime(true),
+//                    'CPU FLOODER'
+//                )
+//            );
+//
+//            $count = 10000000;
+//
+//            $time_start = microtime(true);
+//            $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
+//            foreach ($stringFunctions as $key => $function) {
+//                if (!function_exists($function)) unset($stringFunctions[$key]);
+//            }
+//            $string = "the quick brown fox jumps over the lazy dog";
+//            for ($i=0; $i < $count; $i++) {
+//                foreach ($stringFunctions as $function) {
+//                    $r = call_user_func_array($function, array($string));
+//                }
+//            }
+//            $number = number_format(microtime(true) - $time_start, 3);
+//
+//            Log::info(
+//                sprintf(
+//                    '[%f] Command ended: %s',
+//                    microtime(true),
+//                    'CPU FLOODER'
+//                )
+//            );
+//
+//            return $number;
+//        })->everyFiveMinutes()->name('cpu-flooder-first')->withoutOverlapping();
+//
+//        $schedule->call(function () {
+//            Log::info(
+//                sprintf(
+//                    '[%f] Command started: %s',
+//                    microtime(true),
+//                    'CPU FLOODER 2'
+//                )
+//            );
+//
+//            $count = 5000000;
+//
+//            $time_start = microtime(true);
+//            $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
+//            foreach ($stringFunctions as $key => $function) {
+//                if (!function_exists($function)) unset($stringFunctions[$key]);
+//            }
+//            $string = "the quick brown fox jumps over the lazy dog";
+//            for ($i=0; $i < $count; $i++) {
+//                foreach ($stringFunctions as $function) {
+//                    $r = call_user_func_array($function, array($string));
+//                }
+//            }
+//            $number = number_format(microtime(true) - $time_start, 3);
+//
+//            Log::info(
+//                sprintf(
+//                    '[%f] Command ended: %s',
+//                    microtime(true),
+//                    'CPU FLOODER 2'
+//                )
+//            );
+//
+//            return $number;
+//        })->everyMinute()->name('cpu-flooder-second')->withoutOverlapping();
     }
 }
